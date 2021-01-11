269bd96f0e7aa605d4ebf325271521dba8a20112
Fiona Wee Eng <91084706@g.mvusd.net>
2021-01-11 20:01:53 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Review_Git 
+{
+  public static void main(String[] args) 
+  {
+    Scanner sc = new Scanner(System.in);
+    int num1;
+    System.out.println("Pick an int: ");
+    num1 = sc.nextInt();
+    System.out.println("Today's lucky number is " + num1);
+  }//end main
+}//end class
